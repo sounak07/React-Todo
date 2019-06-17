a8b86d02a3b51fbb39c135f3900da55c0b6aaa3a
sounak07 <sounakume@gmail.com>
2019-06-18 01:07:57 +0530

    Todo component and style add

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoItem = () => {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  );
+};
+
+export default TodoItem;
