de39ca660800489e8db1d4ac819201c5fb6936df
sounak07 <sounakume@gmail.com>
2019-06-19 10:14:01 +0530

    Click handler add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
+/*
+Let's practice props and mapping components on our todo list app!
+
+I've created a js file with some todos data in it, which I'm imported into this file. (Normally this data would come from an API call, not a local file). 
+
+Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
+*/
+
 import React from "react";
 import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedState = prevState.todos.map(todo => {
+        if (todo.id == id) todo.completed = !todo.completed;
+
+        return todo;
+      });
+
+      return {
+        todos: updatedState
+      };
+    });
+  }
+
+  render() {
+    const todos = this.state.todos.map(item => (
+      <TodoItem
+        key={item.id}
+        todo={item}
+        changed={this.handleChange.bind(this, item.id)}
+      />
+    ));
+
+    return <div className="todo-list">{todos}</div>;
+  }
+}
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-const TodoItem = () => {
+function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <input
+        type="checkbox"
+        checked={props.todo.completed}
+        onChange={props.changed}
+      />
+      <p>{props.todo.text}</p>
     </div>
   );
-};
+}
 
 export default TodoItem;
